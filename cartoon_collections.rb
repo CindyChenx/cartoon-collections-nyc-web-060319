7c83027d4953f077a28a574a21cd63ac5977e316
Cindy Chen <chen_cx0128@hotmail.com>
2019-05-30 23:19:42 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,8 @@
-def roll_call_dwarves# code an argument here
+dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"]
+def roll_call_dwarves(dwarves)# code an argument here
   # Your code here
+  dwarves.each do |dwarf|
+    puts dwarf
 end
 
 def summon_captain_planet# code an argument here
