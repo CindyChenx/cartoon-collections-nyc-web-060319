dc88964f01ad4e637febffe552273cae5bc47cbf
Cindy Chen <chen_cx0128@hotmail.com>
2019-05-30 23:21:52 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,8 +5,11 @@ def roll_call_dwarves(dwarves)# code an argument here
     puts dwarf
 end
 
-def summon_captain_planet# code an argument here
+planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
+def summon_captain_planet(planeteer_calls)# code an argument here
   # Your code here
+  planeteer_calls.collect do |x|
+    x.capi
 end
 
 def long_planeteer_calls# code an argument here
